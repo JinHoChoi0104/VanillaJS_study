530a39056fcec5cd0d0b33eb13266c3fcffb8d11
JinHoChoi <bearsff@naver.com>
2021-09-24 00:34:36 +0900

    Nomad assignment
    https://codesandbox.io/s/day-three-blueprint-forked-6ignb?file=/src/index.js

--- Make Chrome App by using Vanilla JS/MOMENTUM/app.js
+++ Make Chrome App by using Vanilla JS/MOMENTUM/app.js
@@ -1,7 +1,39 @@
-const player = {
-    name: "jinho"
+// <⚠️ DONT DELETE THIS ⚠️>
+//import "./styles.css";
+const colors = ["#1abc9c", "#3498db", "#9b59b6", "#f39c12", "#e74c3c"];
+// <⚠️ /DONT DELETE THIS ⚠️>
+
+/*
+✅ The text of the title should change when the mouse is on top of it.
+✅ The text of the title should change when the mouse is leaves it.
+✅ When the window is resized the title should change.
+✅ On right click the title should also change.
+✅ The colors of the title should come from a color from the colors array.
+✅ DO NOT CHANGE .css, or .html files.
+✅ ALL function handlers should be INSIDE of "superEventHandler"
+*/
+let title = document.querySelector("h2");
+
+const superEventHandler = {
+  handleMouseEnter: function () {
+    title.innerText = "The mouse is here!";
+    title.style.color = colors[0];
+  },
+  handleMouseLeave: function () {
+    title.innerText = "The mouse is gone!";
+    title.style.color = colors[1];
+  },
+  handleResize: function () {
+    title.innerText = "You just resized!";
+    title.style.color = colors[2];
+  },
+  handleContextmenu: function () {
+    title.innerText = "That was a right click!";
+    title.style.color = colors[4];
+  },
 };
-console.log(player);
-player.name = "joey";
-plater.lastName = "Choi";
-console.log(player);
+
+title.addEventListener("mouseenter", superEventHandler.handleMouseEnter);
+title.addEventListener("mouseleave", superEventHandler.handleMouseLeave);
+window.addEventListener("resize", superEventHandler.handleResize);
+window.addEventListener("contextmenu", superEventHandler.handleContextmenu);
