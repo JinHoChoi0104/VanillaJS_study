c00b22619557e2bce0dfe42bb7ef913e5155171c
JinHoChoi <bearsff@naver.com>
2021-10-03 01:01:58 +0900

    To do list
    add to do list function

--- Make Chrome App by using Vanilla JS/MOMENTUM/js/backgrounds.js
+++ Make Chrome App by using Vanilla JS/MOMENTUM/js/backgrounds.js
@@ -9,12 +9,6 @@ const images = [
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-//const bgImage = document.createElement("img");
-//bgImage.src = `img/${chosenImage}`;
-//document.body.appendChild(bgImage);
-
-const bgImage = document.createElement("background");
-bgImage.src = red;
-
+const bgImage = document.createElement("img");
+bgImage.src = `img/${chosenImage}`;
 document.body.appendChild(bgImage);
-document.body.style.background =red;
--- Make Chrome App by using Vanilla JS/MOMENTUM/js/greeting.js
+++ Make Chrome App by using Vanilla JS/MOMENTUM/js/greeting.js
@@ -3,17 +3,33 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+//반복되는 스트링은 변수로 선언 해주는 것이 좋다 실수 줄이기 위해
 
-function onLoginSubmit(event){
-  event.preventDefault(); //default event를 실행시키지 않는다
+function onLoginSubmit(event) {
+  event.preventDefault();
+  /*
+  default event!
+  submitd의 기본 동작 (default)은 페이지를 새로고침하는 것이고
+  그것을 막아준다.
+  */
+
+
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  greeting.innerText = "Hello " + username;
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
+}
+
+function paintGreetings(username) {
   greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-
-loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
-
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+if (savedUsername == null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreetings(savedUsername);
+}
